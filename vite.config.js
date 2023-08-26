74e4ef23ccda00ceff3c8a4e912667a2a4f10e66
Josemi Anton <83053474+jantonca@users.noreply.github.com>
2023-08-26 16:47:25 +1000

    Update vite.config.js
    Fixing the base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/clock-javascript/',
+  base: '/clock-react/',
   plugins: [react()],
 })
